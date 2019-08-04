be236e04275804eaca7372f23e49626e190b756e
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2019-08-04 10:03:41 -0700

    Keeps Track Of People In Your Life!

--- controllers/loginHandler.php
+++ controllers/loginHandler.php
@@ -1,4 +1,7 @@
 <?php
+    // includes
+    require '../lib/dbSuper.php';
+    $db = new DbSuper();
     echo 'trying to log you in dawg<br>';
     //echo $_POST['']
 ?>
--- lib/dbSuper.php
+++ lib/dbSuper.php
@@ -1,12 +1,33 @@
 <?php
 class DbSuper
 {
-    // property declaration
-    public $var = 'a default value';
+    // Databse variables 
+    protected $servername;
+    protected $dbusername;
+    protected $dbpassword;
+    protected $dbname;
+    protected $conn;
 
     // Constructor
     function __construct() {
-
+        $this->_initTest();
+        $this->connect();
     } 
+
+    // Initializes the test database
+    function _initTest() {
+        $this->servername = "localhost";
+        $this->dbusername = "root";
+        $this->dbpassword = "";
+        $this->dbname = "test";
+        $this->conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+    }
+
+    // Will initialize the production db
+    function _initProd() {
+        //TODO fill
+    }
+    
+    
 }
 ?>
